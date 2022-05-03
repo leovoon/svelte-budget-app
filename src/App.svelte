7fb40777e0144d84146166143183823a80933437
leovoon <voonlihhaur@gmail.com>
2022-05-03 12:47:23 +0800

    add func

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
   import "carbon-components-svelte/css/all.css"
-  import { Dropdown, Tag, Button, TextInput, NumberInput, RadioButtonGroup, RadioButton, ExpandableTile } from "carbon-components-svelte"
+  import { Dropdown, Form, Tag, Button, TextInput, NumberInput, RadioButtonGroup, RadioButton, ExpandableTile, InlineNotification } from "carbon-components-svelte"
   import Add from "carbon-icons-svelte/lib/Add.svelte"
-  import { budgets, expenses } from "./stores"
+  import { budgets, expenses, addBudget, addExpense } from "./stores"
+  import { v4 as uuidv4 } from "uuid"
+  import { AddFilled } from "carbon-icons-svelte"
+
   let theme: "white" | "g10" | "g80" | "g90" | "g100" = "white"
   // declare theme array with property id and text value
   let themeArray = [
@@ -17,42 +20,70 @@
   let radioSelected = "expense"
   let textInputExpense: string
   let textInputBudget: string
-  let listOfBudgets = $budgets.map((budget) => ({ id: budget.name, text: budget.name }))
-  let selectedBudget = listOfBudgets[0].id
-
+  let numberInputBudget: number = 1
+  let numberInputExpense: number = 1
+  let selectedBudget = $budgets[0].id
   $: textInput = radioSelected === "expense" ? textInputExpense : textInputBudget
+  $: numberInput = radioSelected === "expense" ? numberInputExpense : numberInputBudget
   $: textInputLabel = radioSelected === "expense" ? "what expense" : "what budget"
+  $: listOfBudgets = $budgets.map(({ id, name }) => ({ id, text: name }))
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (radioSelected === "expense") {
+      addExpense({
+        amount: numberInput,
+        name: textInput,
+        budgetId: selectedBudget,
+        timestamp: new Date(Date.now()).toISOString().split("T")[0],
+      })
+      alert("Added expenses")
+    } else {
+      if ($budgets.find(({ name }) => name === textInput)) {
+        alert("Budget already exists")
+        return
+      }
+      addBudget({
+        name: textInput,
+        amount: numberInput,
+        id: uuidv4(),
+      })
+      alert("Added budget")
+    }
+  }
 </script>
 
 <main>
   <h1>Svelte Budget</h1>
   <Dropdown titleText="Select theme" bind:selectedId={theme} items={themeArray} />
   <br />
-  <div style:display="grid" style:gap="1rem">
-    <RadioButtonGroup name="radio-button-group" bind:selected={radioSelected}>
-      <RadioButton id="radio-1" value="budget" labelText="New Budget" />
-      <RadioButton id="radio-2" value="expense" labelText="New Expense" />
-    </RadioButtonGroup>
-    <TextInput labelText={textInputLabel} bind:value={textInput} placeholder="Enter name..." />
+  <Form on:submit={handleSubmit}>
+    <div style:display="grid" style:gap="1rem">
+      <RadioButtonGroup name="radio-button-group" bind:selected={radioSelected}>
+        <RadioButton id="radio-1" value="budget" labelText="New Budget" />
+        <RadioButton id="radio-2" value="expense" labelText="New Expense" />
+      </RadioButtonGroup>
+      <TextInput labelText={textInputLabel} bind:value={textInput} placeholder="Enter name..." />
 
-    <NumberInput min={1} value={1} label="how many (RM)" invalidText="Amount must be greater than 0" />
-    {#if radioSelected === "expense"}
-      <Dropdown direction="top" titleText="which budget" inline bind:selectedId={selectedBudget} items={listOfBudgets} />
-    {/if}
-    <Button style="place-self: flex-end;" expressive kind="secondary" iconDescription="Add" icon={Add} />
-  </div>
-
+      <NumberInput min={1} bind:value={numberInput} label="how many (RM)" invalidText="Amount must be greater than 0" />
+      {#if radioSelected === "expense"}
+        <Dropdown direction="top" titleText="which budget" inline bind:selectedId={selectedBudget} items={listOfBudgets} />
+      {/if}
+      <Button type="submit" style="place-self: flex-end;" expressive kind="secondary" iconDescription="Add" tooltipPosition="left" icon={Add} />
+    </div>
+  </Form>
+  {@debug $expenses}
   <div style:margin-top="2rem" style:display="grid" style:gap="1rem">
     <div style:display="flex" style:justify-content="space-between" style:align-items="center">
       <h4>History</h4>
       <Tag type="purple">RM {0}</Tag>
     </div>
 
-    {#each $budgets as { name: budgetName, amount }}
+    {#each $budgets as { id, name, amount }}
       <ExpandableTile tileMaxHeight={10} tileCollapsedIconText="Interact to expand budget" tileExpandedIconText="Interact to collapse budget">
         <div slot="above" style:height="3rem">
           <div style:display="flex" style:align-items="center" style:gap="10px">
-            <h5>{budgetName}</h5>
+            <h5>{name}</h5>
             <Tag type="cyan" size="sm">RM {amount}</Tag>
           </div>
         </div>
@@ -60,9 +91,9 @@
         <div slot="below" style:height="10rem">
           <h5>Expenses</h5>
           <div style:height="10rem">
-            {#each $expenses as { name, budget, amount, timestamp }}
-              {#if budgetName === budget}
-                <div style:height="10rem">
+            {#each $expenses as { name, budgetId, amount, timestamp }}
+              {#if id === budgetId}
+                <div style:display="block">
                   {name}
                   <Tag type="magenta">RM {amount}</Tag>
                   <Tag type="cool-gray">{timestamp}</Tag>
--- src/stores.ts
+++ src/stores.ts
@@ -1,14 +1,24 @@
 import { writable } from "svelte/store";
 
+
 export const budgets = writable([
-    { name: "Food", amount: 400 },
-    { name: "Clothes", amount: 20 },
-    { name: "Entertainment", amount: 20 },
-    { name: "Transportation", amount: 100 },
-    { name: "Health", amount: 0 },
+    { id: 0, name: "Food", amount: 400 },
+    { id: 1, name: "Clothes", amount: 20 },
+    { id: 2, name: "Entertainment", amount: 20 },
+    { id: 3, name: "Transportation", amount: 100 },
+    { id: 4, name: "Health", amount: 0 },
 ]);
+
+export function addBudget(budget){
+    budgets.update(budgets => [...budgets, budget]);
+}
+
 export const expenses = writable([
-    { name: "eat", amount: 10 , budget: "Food" , timestamp: "2020-01-01" },
-    { name: "uniqlo", amount: 60 , budget: "Clothes" , timestamp: "2020-01-01" },
-    { name: "movie", amount: 20 , budget: "Entertainment" , timestamp: "2020-01-01" },
+    { name: "eat", amount: 10 , budgetId: 0 , timestamp: "2020-01-01" },
+    { name: "uniqlo", amount: 60 , budgetId: 1 , timestamp: "2020-01-01" },
+    { name: "movie", amount: 20 , budgetId: 2 , timestamp: "2020-01-01" },
 ]);
+
+export function addExpense(expense){
+    expenses.update(expenses => [...expenses, expense]);
+}
