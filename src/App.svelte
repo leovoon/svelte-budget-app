c889092c0d72bafe209ef4e7af0b4e3b235d2797
leovoon <voonlihhaur@gmail.com>
2022-05-04 18:56:16 +0800

    add uncategorized

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,10 @@
 <script lang="ts">
   import "carbon-components-svelte/css/all.css"
-  import { Dropdown, ProgressBar, Form, Tag, Button, TextInput, NumberInput, RadioButtonGroup, RadioButton, ExpandableTile, InlineNotification } from "carbon-components-svelte"
+  import { Dropdown, Slider, ProgressBar, Form, Tag, Button, TextInput, NumberInput, RadioButtonGroup, RadioButton, ExpandableTile, InlineNotification } from "carbon-components-svelte"
   import Add from "carbon-icons-svelte/lib/Add.svelte"
   import ChevronDown from "carbon-icons-svelte/lib/ChevronDown.svelte"
   import ChevronUp from "carbon-icons-svelte/lib/ChevronUp.svelte"
-
-  import { budgets, expenses, addBudget, addExpense } from "./stores"
+  import { budgets, expenses, addBudget, addExpense, UncategorizedID } from "./stores"
   import { v4 as uuidv4 } from "uuid"
 
   let theme: "white" | "g10" | "g80" | "g90" | "g100" = "white"
@@ -19,9 +18,10 @@
   let radioSelected = "expense"
   let textInputExpense: string
   let textInputBudget: string
+  let textInput = ""
   let numberInputBudget: number = 1
   let numberInputExpense: number = 1
-  let selectedBudget = $budgets[0].id
+  let selectedBudget = UncategorizedID
   let expanded = false
 
   $: document.documentElement.setAttribute("theme", theme)
@@ -49,7 +49,7 @@
 
   function handleSubmit(e) {
     e.preventDefault()
-    if (textInput.trim() === "" || textInput === undefined) {
+    if (textInput?.trim() === "" || textInput === undefined) {
       alert("Empty name no good")
       return
     }
@@ -87,8 +87,12 @@
         <RadioButton id="radio-2" value="expense" labelText="New Expense" />
       </RadioButtonGroup>
       <TextInput labelText={textInputLabel} bind:value={textInput} placeholder="Enter name..." />
+      {#if radioSelected === "expense"}
+        <NumberInput min={1} bind:value={numberInput} label="how many (RM)" invalidText="Amount must be greater than 0" />
+      {:else}
+        <Slider step={10} min={10} max={1000} bind:value={numberInput} labelText="max budget (RM)" />
+      {/if}
 
-      <NumberInput min={1} bind:value={numberInput} label="how many (RM)" invalidText="Amount must be greater than 0" />
       {#if radioSelected === "expense"}
         <Dropdown direction="top" titleText="which budget" inline bind:selectedId={selectedBudget} items={listOfBudgets} />
       {/if}
@@ -106,35 +110,41 @@
         <Tag type="purple">RM {totalBudget}</Tag>
       </div>
     </div>
-    <ProgressBar bind:value={totalExpense} bind:max={totalBudget} labelText={`${budgetLeft} available ${totalExpense} used`} />
 
-    {#each $budgets as { id, name, amount }}
-      <ExpandableTile {expanded} tileMaxHeight={10} tileCollapsedIconText="Interact to expand budget" tileExpandedIconText="Interact to collapse budget">
-        <div slot="above" style:height="3rem">
-          <div class="flex-center" style:gap="10px">
-            <h5>{name}</h5>
-            <Tag type="cyan" size="sm">RM {amount}</Tag>
+    {#if $budgets.length > 0}
+      <ProgressBar bind:value={totalExpense} bind:max={totalBudget} labelText={`${budgetLeft} available ${totalExpense} used`} />
+      {#each $budgets as { id, name, amount }}
+        <ExpandableTile {expanded} tileMaxHeight={10} tileCollapsedIconText="Interact to expand budget" tileExpandedIconText="Interact to collapse budget">
+          <div slot="above" style:height="3rem">
+            <div class="flex-center" style:gap="10px">
+              <h5>{name}</h5>
+              <Tag type="cyan" size="sm">RM {amount}</Tag>
+            </div>
           </div>
-        </div>
 
-        <div slot="below" style:overflow-y="auto" style:height="10rem">
-          <h5>Expenses</h5>
-          <ProgressBar value={totalExpensesByBudget(id)} max={amount} labelText={`${budgetLeftByBudget(id)} available ${totalExpensesByBudget(id)} used`} />
+          <div slot="below" style:overflow-y="auto" style:height="10rem">
+            <h5>Expenses</h5>
+            <ProgressBar value={totalExpensesByBudget(id)} max={amount} labelText={id === UncategorizedID ? `${totalExpensesByBudget(id)} used` : `${budgetLeftByBudget(id)} available ${totalExpensesByBudget(id)} used`} />
 
-          <div class="expenses">
-            {#each $expenses as { name, budgetId, amount, timestamp }}
-              {#if id === budgetId}
-                <div style:display="block">
-                  {name}
-                  <Tag type="magenta">RM {amount}</Tag>
-                  <Tag type="cool-gray">{timestamp}</Tag>
-                </div>
-              {/if}
-            {/each}
+            <div class="expenses">
+              {#each $expenses as { name, budgetId, amount, timestamp }}
+                {#if id === budgetId}
+                  <div style:display="block">
+                    {name}
+                    <Tag type="magenta">RM {amount}</Tag>
+                    <Tag type="cool-gray">{timestamp}</Tag>
+                  </div>
+                {/if}
+              {/each}
+            </div>
           </div>
-        </div>
-      </ExpandableTile>
-    {/each}
+        </ExpandableTile>
+      {/each}
+    {:else}
+      <div style:display="block">
+        <h5 style:text-align="center">No budgets yet</h5>
+      </div>
+    {/if}
   </div>
 </main>
 
@@ -154,6 +164,18 @@
     margin-block: 1rem;
   }
 
+  :global(.bx--slider__range-label) {
+    display: none;
+  }
+
+  :global(.bx--slider-container) {
+    width: 100%;
+    justify-content: space-between;
+  }
+  :global(.bx--text-input, .bx--slider-text-input) {
+    min-width: max-content;
+  }
+
   .flex-center {
     display: flex;
     align-items: center;
@@ -168,5 +190,11 @@
   footer {
     text-align: center;
     padding: 1em;
+  }
+
+  @media (min-width: 600px) {
+    main {
+      width: 60ch;
+    }
   }
 </style>
--- src/stores.ts
+++ src/stores.ts
@@ -1,22 +1,29 @@
-import { writable } from "svelte/store";
+import { readable, writable } from "svelte/store";
 
+export const UncategorizedID = 'Uncategorized';
 
 export const budgets = writable([
-    { id: 0, name: "Food", amount: 400 },
-    { id: 1, name: "Clothes", amount: 20 },
-    { id: 2, name: "Entertainment", amount: 20 },
-    { id: 3, name: "Transportation", amount: 100 },
-    { id: 4, name: "Health", amount: 0 },
+    {id: UncategorizedID, name: "Uncategorized", amount: 0},
+    // { id: 0, name: "Food", amount: 400 },
+    // { id: 1, name: "Clothes", amount: 20 },
+    // { id: 2, name: "Entertainment", amount: 20 },
+    // { id: 3, name: "Transportation", amount: 100 },
+    // { id: 4, name: "Health", amount: 0 },
 ]);
 
 export function addBudget(budget){
+    if(budget.id === UncategorizedID){
+       budgets.update(budgets => 
+        budgets.find(b => b.id === UncategorizedID).amount += budget.amount
+       )
+    }
     budgets.update(budgets => [...budgets, budget]);
 }
 
 export const expenses = writable([
-    { name: "eat", amount: 10 , budgetId: 0 , timestamp: "2020-01-01" },
-    { name: "uniqlo", amount: 60 , budgetId: 1 , timestamp: "2020-01-01" },
-    { name: "movie", amount: 20 , budgetId: 2 , timestamp: "2020-01-01" },
+    // { name: "eat", amount: 10 , budgetId: 0 , timestamp: "2020-01-01" },
+    // { name: "uniqlo", amount: 60 , budgetId: 1 , timestamp: "2020-01-01" },
+    // { name: "movie", amount: 20 , budgetId: 2 , timestamp: "2020-01-01" },
 ]);
 
 export function addExpense(expense){
